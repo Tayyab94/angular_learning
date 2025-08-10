532371e115bc0aa9bd223b71a1b901ac2e8adaa8
Tayyabvista4 <tayyab@vista4ventures.com>
2025-08-10 22:56:43 +0500

    here we leanr how to pass data from parent to child

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfile } from './components/user-profile/user-profile';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfile],
+  imports: [RouterOutlet, UserProfile, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+
+
+   users = [
+  { name: "Tayyab", isSingle: true, salary: 1000 },
+  { name: "Ahmed", isSingle: false, salary: 1500 },
+  { name: "Sara", isSingle: true, salary: 1200 },
+  { name: "Ali", isSingle: false, salary: 1800 },
+  { name: "Fatima", isSingle: true, salary: 950 },
+  { name: "Usman", isSingle: false, salary: 2000 },
+  { name: "Aisha", isSingle: true, salary: 1100 }
+];
   protected readonly title = signal('my-first-angular-app');
 }
--- src/app/components/user-profile/user-profile.ts
+++ src/app/components/user-profile/user-profile.ts
@@ -1,49 +1,47 @@
-import { Component, Input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+  import { Attribute, booleanAttribute, Component, Input, numberAttribute } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
 
-function GreetingFunction(value:string)
-{
-  return "Hi..!"+value
-}
-
-@Component({
-  selector: 'app-user-profile',
-  imports: [FormsModule],
-  templateUrl: './user-profile.html',
-  styleUrl: './user-profile.css'
-})
-export class UserProfile {
-
-  @Input({alias:"userkiAge"}) userAge: number=0;
-  @Input({alias:"userkiName",transform:GreetingFunction}) userName: string = "";
-  name: string="Tayyab"
-  status="Active"
-  salary: number=23.3
-
-  address: string=""
-
-  isButtonDisabled=false
-
- users = [
-  { name: "Tayyab", isSingle: true, salary: 1000 },
-  { name: "Ahmed", isSingle: false, salary: 1500 },
-  { name: "Sara", isSingle: true, salary: 1200 },
-  { name: "Ali", isSingle: false, salary: 1800 },
-  { name: "Fatima", isSingle: true, salary: 950 },
-  { name: "Usman", isSingle: false, salary: 2000 },
-  { name: "Aisha", isSingle: true, salary: 1100 }
-];
-
-  OnChange(e:Event)
+  function GreetingFunction(value:string)
   {
-    const inputValue= (e.target as HTMLInputElement ).value
-    this.name= inputValue
-    console.log(inputValue)
+    return "Hi..!"+value
   }
 
-   ButtonClick = async()=>{
-      if(this.name=="admin" && this.status=="Active"){
-        alert("Welcome Acdmmin")
+  @Component({
+    selector: 'app-user-profile',
+    imports: [FormsModule],
+    templateUrl: './user-profile.html',
+    styleUrl: './user-profile.css'
+  })
+  export class UserProfile {
+
+    @Input({alias:"userkiAge"}) userAge: number=0;
+    @Input({alias:"userkiName",transform:GreetingFunction}) userName: string = "";
+    @Input({transform: numberAttribute}) customerSalary!: number;
+
+    @Input({transform:booleanAttribute}) isCustomerActive!: boolean;
+
+    name: string="Tayyab"
+    status="Active"
+    salary: number=23.3
+
+    address: string=""
+
+    isButtonDisabled=false
+
+
+
+  @Input() userData!:any[]
+  
+    OnChange(e:Event)
+    {
+      const inputValue= (e.target as HTMLInputElement ).value
+      this.name= inputValue
+      console.log(inputValue)
+    }
+
+    ButtonClick = async()=>{
+        if(this.name=="admin" && this.status=="Active"){
+          alert("Welcome Acdmmin")
+    }
   }
-}
-}
+  }
