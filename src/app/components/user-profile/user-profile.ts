209d296a35d08aa00e774ac31c42f289c6ea4db8
Tayyabvista4 <tayyab@vista4ventures.com>
2025-08-16 03:42:49 +0500

    custom directive, lifecycle, done

--- src/app/components/user-profile/user-profile.ts
+++ src/app/components/user-profile/user-profile.ts
@@ -1,8 +1,11 @@
-  import { Attribute, booleanAttribute, Component, EventEmitter, Input, numberAttribute, Output } from '@angular/core';
+  import { AfterViewInit, Attribute, booleanAttribute, Component, ElementRef, 
+    EventEmitter, Input, numberAttribute, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, 
+    ViewChild} from '@angular/core';
   import { FormsModule } from '@angular/forms';
   import { UserModel } from "../../../models/user.model"
 import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { CountryCodePipe } from '../../pipes/country-code-pipe';
+import {Highlighted} from "../../directives/highlighted"
   function GreetingFunction(value:string)
   {
     return "Hi..!"+value
@@ -10,12 +13,34 @@ import { CountryCodePipe } from '../../pipes/country-code-pipe';
 
   @Component({
     selector: 'app-user-profile',
-    imports: [FormsModule, TitleCasePipe, UpperCasePipe, CountryCodePipe],
+    imports: [FormsModule, TitleCasePipe, UpperCasePipe, CountryCodePipe,Highlighted],
     templateUrl: './user-profile.html',
     styleUrl: './user-profile.css'
   })
-  export class UserProfile {
+  export class UserProfile implements OnInit, OnDestroy, OnChanges, AfterViewInit {
 
+
+   
+    @ViewChild("myCustomerSalary")custSalary?: ElementRef
+
+     ngAfterViewInit(): void {
+      alert(this.custSalary?.nativeElement.textContent)
+        console.log('ngAfterViewInit '+ this.custSalary?.nativeElement.textContent);
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+      console.log(changes)
+    }
+  
+    ngOnDestroy(): void {
+      // unregister the event listener
+        console.log('ngOnDestroy called')
+    }
+    constructor(){
+
+    }
+    ngOnInit(){
+      console.log("ngOnInit called");
+    }
     @Input({alias:"userkiAge"}) userAge: number=0;
     @Input({alias:"userkiName",transform:GreetingFunction}) userName: string = "";
     @Input({transform: numberAttribute}) customerSalary!: number;
